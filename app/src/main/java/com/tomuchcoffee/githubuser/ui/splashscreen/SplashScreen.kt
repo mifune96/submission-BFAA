41c3ebccc435c7054cd82a26b63983a0b172f9b0
mifune96 <imrank.sensea@gmail.com>
2022-01-04 09:38:20 +0700

    add splashscreen

---
+++ app/src/main/java/com/tomuchcoffee/githubuser/ui/splashscreen/SplashScreen.kt
@@ -0,0 +1,28 @@
+package com.tomuchcoffee.githubuser.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.tomuchcoffee.githubuser.databinding.ActivitySplashScreenBinding
+import com.tomuchcoffee.githubuser.ui.main.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
